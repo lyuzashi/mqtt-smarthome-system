ed03650a753ca2984a81f9a53989de23637beb82
Ben Patterson <ben@robotjamie.com>
2018-08-16 22:20:53 +1000

    Lookup root

--- system/git/git-update.mjs
+++ system/git/git-update.mjs
@@ -1,6 +1,7 @@
 import Git from 'simple-git/promise';
+import root from '../../root';
 
-export const git = Git();
+export const git = Git(root);
 
 /* This could be smarter by fetching and merging the specific commit the hook
 triggered, but for all intents and purposes, this is the same thing */
