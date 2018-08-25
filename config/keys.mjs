393ea5c8b9d339ef48332f37a1ac6ce603bbafe1
Ben Patterson <ben@robotjamie.com>
2018-08-26 09:56:15 +1000

    Use webdav for config

--- config/keys.mjs
+++ config/keys.mjs
@@ -1,12 +1,21 @@
-import https from 'https';
+import WebDavFS from 'webdav-fs';
+import { promisify } from 'util';
 
-const url = 'https://kryten.grid.robotjamie.com/config/mqtt-smarthome-system.json';
+const url = 'https://kryten.grid.robotjamie.com/config/';
+const filename = 'mqtt-smarthome-system.json';
 
-export default new Promise((resolve, reject) => {
-  https.get(url, response => {
-    let data = '';
-    response.on('data', chunk => data += chunk);
-    response.on('end', () => resolve(JSON.parse(data)));
-  })
-  .on('error', error => reject(error));
-});
+const fs = WebDavFS(url);
+
+const read = promisify(fs.readFile);
+const write = promisify(fs.writeFile);
+
+export const get = (key) => read(filename)
+  .then(JSON.parse)
+  .then(keys => key ? keys[key] : keys);
+
+export const set = (key, value) => get()
+  .then(document => Object.assign(document || {}, { [key]: value }))
+  .then(JSON.stringify)
+  .then(document => write(filename, document));
+
+export default get();
