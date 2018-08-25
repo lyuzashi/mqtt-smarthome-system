10a1b074c120171f4bb09624bc891368b48916f6
Ben Patterson <ben@robotjamie.com>
2018-08-25 14:54:29 +1000

    maybe update characteristic

--- interfaces/hap.mjs
+++ interfaces/hap.mjs
@@ -51,7 +51,10 @@ Object.keys(config).forEach(namespace => {
           case 'get':
             // Requires mapping
             subscriptions.map(event.topic, event.map);
-            mqtt.subscribe(event.topic, (topic, value) => subscriptions.set(event.topic, value));
+            mqtt.subscribe(event.topic, (topic, value) => {
+              subscriptions.set(event.topic, value);
+              service.updateCharacteristic(characteristic, subscriptions.get(event.topic));
+            });
             characteristic.on(eventName, callback => {
               if (subscriptions.has(event.topic)) {
                 return callback(null, subscriptions.get(event.topic));
