d41040418777a83619a9b4740e41e28458eb166c
Ben Patterson <ben@robotjamie.com>
2018-09-22 10:27:33 +1000

    Basic console panel

--- visualizations/web/app.jsx
+++ visualizations/web/app.jsx
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PanelGroup from 'react-panelgroup';
 import Body from './components/body';
 import XTerm from './components/xterm';
 
 export default class App extends Component {
   render() {
-    return <div>
-      <Body />
-      <XTerm />
-    </div>
+    return (
+      <Fragment>
+        <Body />
+        <PanelGroup direction="column">
+          <div>Something else</div>
+          <div style={{width: '100%', height: '100%'}}>
+            <XTerm />
+          </div>
+        </PanelGroup>
+      </Fragment>
+    );
   }
 }
