1f983d50da45c6ee53df0a3f9c8cf35b93a466f9
Ben Patterson <ben@robotjamie.com>
2018-10-04 17:33:32 +1000

    Setup LE

--- visualizations/web/ssl.mjs
+++ visualizations/web/ssl.mjs
@@ -2,11 +2,11 @@ import createServer from 'auto-sni';
 import app from './app';
 
 const options = {
-  email: ..., // Emailed when certificates expire.
+  email: 'ben@robotjamie.com',
   agreeTos: true, // Required for letsencrypt.
   debug: true, // Add console messages and uses staging LetsEncrypt server. (Disable in production)
-  domains: ["mysite.com", ["test.com", "www.test.com"]], // List of accepted domain names. (You can use nested arrays to register bundles with LE).
-  dir: "~/letsencrypt/etc", // Directory for storing certificates. Defaults to "~/letsencrypt/etc" if not present.
+  domains: ['hal9000.grid.robotjamie.com'], // List of accepted domain names. (You can use nested arrays to register bundles with LE).
+  // dir: "~/letsencrypt/etc", // Directory for storing certificates. Defaults to "~/letsencrypt/etc" if not present.
   ports: {
     http: 80, // Optionally override the default http port.
     https: 443 // // Optionally override the default https port.
@@ -14,3 +14,6 @@ const options = {
 }
 
 export default createServer(options, app);
+
+// Switch to this when not running on hal9000
+// export default app.listen(8080);
