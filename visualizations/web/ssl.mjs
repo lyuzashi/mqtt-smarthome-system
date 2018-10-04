12af7fe96111292c0204cd542a905157106f4e4d
Ben Patterson <ben@robotjamie.com>
2018-10-04 19:03:17 +1000

    Move to prod LE

--- visualizations/web/ssl.mjs
+++ visualizations/web/ssl.mjs
@@ -3,15 +3,9 @@ import app from './app';
 
 const options = {
   email: 'ben@robotjamie.com',
-  agreeTos: true, // Required for letsencrypt.
-  debug: true, // Add console messages and uses staging LetsEncrypt server. (Disable in production)
-  domains: ['hal9000.grid.robotjamie.com'], // List of accepted domain names. (You can use nested arrays to register bundles with LE).
-  // dir: "~/letsencrypt/etc", // Directory for storing certificates. Defaults to "~/letsencrypt/etc" if not present.
-  ports: {
-    http: 80, // Optionally override the default http port.
-    https: 443 // // Optionally override the default https port.
-  }
-}
+  agreeTos: true,
+  domains: ['hal9000.grid.robotjamie.com'],
+};
 
 export default createServer(options, app);
 
