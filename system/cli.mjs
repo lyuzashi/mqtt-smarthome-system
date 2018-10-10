305164116e9a2d32cf484c1f19c816824864beeb
Ben Patterson <ben@robotjamie.com>
2018-10-10 11:45:54 +1100

    Shell

---
+++ system/cli.mjs
@@ -0,0 +1,3 @@
+import '../main';
+import { start } from './shell';
+start();
--- system/shell.mjs
+++ system/shell.mjs
@@ -1,9 +1,24 @@
 import REPL from 'repl';
+import EventEmitter from 'events';
 
-export const context = {};
+class Context extends EventEmitter{
+  constructor() {
+    super();
+    const self = this;
+    return new Proxy(this, {
+      set(target, property, value) {
+        self.emit('set', { [property]: value });
+        return Reflect.set(target, property, value);
+      }
+    });
+  }
+}
 
-export const start = ({ input = process.stdin, output = process.stdout }) => {
+export const context = new Context();
+
+export const start = ({ input = process.stdin, output = process.stdout } = {}) => {
   const repl = REPL.start({ prompt: '> ', input, output, terminal: true, useGlobal: true });
   Object.assign(repl.context, context);
+  context.on('set', additionalContext => Object.assign(repl.context, additionalContext));
   return repl;
 };
