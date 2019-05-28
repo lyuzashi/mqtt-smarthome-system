24a57307e100d8a0c00b18065daea36e13967352
Ben Patterson <ben@robotjamie.com>
2019-05-28 12:35:09 +1000

    Trailing edge throttling on Hue calls

--- interfaces/hue/request-save.mjs
+++ interfaces/hue/request-save.mjs
@@ -1,14 +1,50 @@
+import assign from 'assign-deep';
 
 const pendingUpdates = new WeakMap();
+const latestState = new Map();
 
 const ENQUEUED = Symbol('enqueued');
+const PENDING = Symbol('pending');
+const THROTTLED = Symbol('throttled');
+
+const attributeMap = {
+  on: 'on',
+  bri: 'brightness',
+  colormode: 'colorMode',
+  hue: 'hue',
+  sat: 'saturation',
+  xy: 'xy',
+  ct: 'colorTemp',
+  alert: 'alert',
+  effect: 'effect',
+  transitiontime: 'transitionTime',
+  bri_inc: 'incrementBrightness',
+  hue_inc: 'incrementHue',
+  sat_inc: 'incrementSaturation',
+  xy_inc: 'incrementXy',
+  ct_inc: 'incrementColorTemp',
+};
+
+const shallowState = light => Object.keys(light.state.changed).reduce((attributes, attribute) =>
+  Object.assign(attributes, { [attributeMap[attribute]]: light.state.attributes[attribute] }), {});
+
+const updateAttributes = light => {
+  const attributes = latestState.get(light);
+  return Object.keys(attributes).reduce((changed, attribute) => {
+    const value = attributes[attribute];
+    if (light[attribute] !== value) {
+      light[attribute] = value;
+      return true;
+    }
+    return changed;
+  }, false);
+}
 
 export default (device, fixture) => {
   // Device is the entire hue.lights object which is required to send the .save command to
   // Fixture is an individual light with changed attributes.
-  // The goal of this function is to debounce the changes for any particular light (and perhaps
-  // the entire hue interface?) so that only the last change in a quick succession is applied
   const queue = createQueue(device);
+  latestState.set(fixture, shallowState(fixture)); // Only necessary if queue is enqueued?
   queue.add(fixture);
   enqueueSave(device);
 }
@@ -17,6 +53,8 @@ const createQueue = (device) => {
   if(pendingUpdates.has(device)) return pendingUpdates.get(device);
   const queue = new Set();
   Object.defineProperty(queue, ENQUEUED, { value: false, writable: true });
+  Object.defineProperty(queue, PENDING, { value: false, writable: true });
+  Object.defineProperty(queue, THROTTLED, { value: false, writable: true });
   pendingUpdates.set(device, queue);
   return queue;
 }
@@ -26,24 +64,33 @@ const enqueueSave = (device) => {
   if (!queue) return;
   const enqueued = queue[ENQUEUED];
   if (enqueued) {
-     console.log('Could not queue change, backed up', device, queue);
-    return; // Rather than blocking, the last change should be saved for sending after queue drain
+     // Flag that changes have been made while requests are in flight
+    queue[THROTTLED] = true;
+    return;
   }
   queue[ENQUEUED] = true;
   // This could also manage auto rate limiting (and open up the possibility of switching
   // to entertainment API automatically when rate is exceeded for compatible lights)
-  // Queue should also remove duplicate changes and ensure last received value is definitely set
-  // (avoid jumping backwards)
   setTimeout(() => {
     const fixtureQueue = [...queue];
-    // queue[ENQUEUED] = false; // Allow further queuing while sending request
-    // console.dir(fixtureQueue.map(fixture => 
-    //   `${fixture.name}: ${Object.keys(fixture.state.changed).map(c => `${c}: ${fixture.state.attributes[c]}`)}`) );
     Promise.all(fixtureQueue.map(fixture => 
       device.save(fixture)
       // Catch 201 parameter, <parameter>, is not modifiable. Device is set to off.
       .catch(error => { if (error.type !== 201) throw error })
-      .then(() => queue.delete(fixture))
-    )).then(() => (queue[ENQUEUED] = false))
+      .then(() => {
+        if(updateAttributes(fixture)) {
+          console.log('running final update', latestState.get(fixture));
+          queue[PENDING] = true;
+        } else {
+          queue.delete(fixture);
+        }
+      })
+    )).then(() => {
+      queue[ENQUEUED] = false
+      if(queue[PENDING]) {
+        enqueueSave(device);
+      }
+      queue[PENDING] = false;
+    })
   })
 }
