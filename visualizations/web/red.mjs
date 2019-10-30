e5cbf612a57dcae53e3a43fe458df1ed3189a7d3
Ben Patterson <ben@robotjamie.com>
2019-10-30 11:45:33 +1100

    Fix red

--- visualizations/web/red.mjs
+++ visualizations/web/red.mjs
@@ -2,8 +2,10 @@ import RED from '../../system/red';
 import server from './ssl';
 import app from '../../system/web';
 
-// Serve the editor UI from /red
-app.use(RED.settings.httpAdminRoot,RED.httpAdmin);
+app.use(RED.settings.httpAdminRoot, RED.httpAdmin);
 
-// Serve the http nodes UI from /api
-app.use(RED.settings.httpNodeRoot,RED.httpNode);
+app.use(RED.settings.httpNodeRoot, RED.httpNode);
+
+app.use('/red/*', (req, res) => {
+  res.end();
+});
